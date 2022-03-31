445e0fd33f13cb86453b9ee55702696a51940194
konstantin <k.saraev@gmail.com>
2022-03-31 10:38:28 +0300

    Added fraud service
    created maven module pom.xml
    added jpa and spring starter web dependencies
    updated parent pom.xml
    added FraudCheckHistory model
    configured server ports, postgresql datasource and jpa in application.yml
    added repository, service, controller
    implemented fraud service ability to check fraudulent customers

---
+++ fraud/src/main/java/com/example/fraud/FraudCheckResponse.java
@@ -0,0 +1,8 @@
+package com.example.fraud;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class FraudCheckResponse {
+  private Boolean isFraudster;
+}
---
+++ fraud/src/main/java/com/example/fraud/FraudController.java
@@ -0,0 +1,18 @@
+package com.example.fraud;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "api/v1/fraud-check")
+@AllArgsConstructor
+public class FraudController {
+
+  private final FraudCheckService fraudCheckService;
+
+  @GetMapping(path = "{customerId}")
+  public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
+    boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+    return new FraudCheckResponse(isFraudulentCustomer);
+  }
+}
