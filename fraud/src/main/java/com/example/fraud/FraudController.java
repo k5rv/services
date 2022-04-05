cdd354ffca947a2d914eb70d17768323ef5000dd
konstantin <k.saraev@gmail.com>
2022-04-05 14:02:00 +0300

    Configured sleuth

--- fraud/src/main/java/com/example/fraud/FraudController.java
+++ fraud/src/main/java/com/example/fraud/FraudController.java
@@ -2,8 +2,10 @@ package com.example.fraud;
 
 import com.example.clients.fraud.FraudCheckResponse;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping(path = "api/v1/fraud-check")
 @AllArgsConstructor
@@ -14,6 +16,7 @@ public class FraudController {
   @GetMapping(path = "{customerId}")
   public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
     boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+    log.info("fraud check request for customer {}", customerId);
     return new FraudCheckResponse(isFraudulentCustomer);
   }
 }
